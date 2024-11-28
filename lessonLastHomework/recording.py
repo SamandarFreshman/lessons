145b5f3894a514bcc5186dc7e7c0e03823068535
Samandar-2006 <asamandar2006@gmail.com>
2024-11-28 23:58:09 +0500

    last

--- lessonLastHomework/recording.py
+++ lessonLastHomework/recording.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
 import os
+import time
 load_dotenv()
 
 file = os.environ.get('HISTORY')
 
 def recording(msg):
     with open(file, 'at') as f:
-        f.write(msg+"\n")
+        f.write(time.datetime.gettimenow()+msg+"\n")
